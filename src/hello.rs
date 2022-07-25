1b49878f64b129ce394c03d3ec93752a5ee36734
jabong111 <jabong111@gmail.com>
2022-07-25 21:47:51 +0900

    Update hello.rs

--- src/hello.rs
+++ src/hello.rs
@@ -1,7 +1,9 @@
+//in github
 fn main(){
 	println!("hello world!");
 	println!("hello world!");
 	println!("hello world!");
 	println!("hello world!");
 	println!("hello master world!");
+	println!("hello master world! in remote repository");
 }
