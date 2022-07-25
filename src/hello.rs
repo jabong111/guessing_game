681f89ee1e09dfdb72107e0b2c8a9e4d7a1cb014
jabong111 <jabong111@gmail.com>
2022-07-25 13:55:06 +0100

    pull 전에 바로 커밋하기 테스트:

--- src/hello.rs
+++ src/hello.rs
@@ -1,7 +1,8 @@
+// in local
 fn main(){
 	println!("hello world!");
 	println!("hello world!");
 	println!("hello world!");
 	println!("hello world!");
-	println!("hello master world!");
+	println!("hello master world! in local");
 }
